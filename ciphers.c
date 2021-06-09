a1caebee2f93aea7946c4f990c2af34589ba4749
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2021-06-09 11:02:50 +0530

    Update ciphers.c

--- ciphers.c
+++ ciphers.c
@@ -30,15 +30,40 @@ void rot(const char s1[],int n)
    c=ch;
   }
 }
-void ving(const char text[],const char key[])
+void ving(const char text[],const char key[],int dec)
 {
   int l=strlen(text);
-  int i=0;
+  int i=0,j=0;
   char key2[l];
+  char encrypted[l];
+  for(i=0;i<l;i++)
+  {
+    key2[i]=key[j];
+    //printf("%s\n",key2);
+    if (j>=strlen(key)-1)
+    {
+      j=j-strlen(key);
+    }
+    j++;
+  }
+  //printf("%s\n",key2);
   for (i=0;i<l;i++)
   {
-    //Work on this 
-    key2[i]=key[i];
-
+    char x;
+    if (!dec)
+    {
+    x=(text[i]+key2[i])%26;
+    //printf("text[i]=%d key2[i]=%d sum=%d mod=%d \t%d %c\n",text[i],key2[i],(key2[i]+text[i])>127?-128+(key2[i]+text[i]):key2[i]+text[i],((key2[i]+text[i])>127?-128+(key2[i]+text[i]):key2[i]+text[i])%26,x,x);
+    x+='A';
+    }
+    else
+    {
+      x=(text[i]-key2[i]+26)%26;
+      x+='A';
+    }
+    //printf("text [i]=%c %d key=%c %d x=%d %c\n",text[i],text[i],key2[i],key2[i],x,x);
+    encrypted[i]=x;
   }
+  encrypted[i]='\0';
+  printf("%s\n",encrypted);
 }
